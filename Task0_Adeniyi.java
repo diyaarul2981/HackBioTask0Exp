b50417a80cfde275c25a2ede0a71c809f05e7ea0
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-06 23:24:27 +0530

    Update Task0_Adeniyi.java

--- Task0_Adeniyi.java
+++ Task0_Adeniyi.java
@@ -4,16 +4,11 @@ public class Program
     {
         String slack="@Oluwabunmi", twitter="@Firstlady1";
         int count=0, x;
-        System.out.println( "Oluwabunmi Adeniyi");
-        System.out.println ("oluwabunmipra@gmail.com");
-        System.out.println ("@Oluwabunmi");
-        System.out.println ("Public Health and Genomic Epidemiology");
-        System.out.println ("FirstLady1");
         for(x=0;x<slack.length();x++)
         {
             if(slack.charAt(x)!=twitter.charAt(x))
             count++;
         }
-        System.out.println("Hamming Distance: "+count);
+        System.out.println( "Oluwabunmi Adeniyi, oluwabunmipra@gmail.com, @Oluwabunmi, Public Health and Genomic Epidemiology, FirstLady1, "+count);
     }
 }
