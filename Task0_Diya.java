0c347541bdc43ea1f5dc592a67251a6e10031061
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-06 23:32:19 +0530

    Update Task0_Diya.java

--- Task0_Diya.java
+++ Task0_Diya.java
@@ -6,16 +6,11 @@ public class Task0_Diya
     {
         String slack="@DiyaArul", twitter="@ArulDiya";
         int count=0, i;
-        System.out.println("Diya Arul");
-        System.out.println("diyaarul1234@gmail.com");
-        System.out.println("@DiyaArul");
-        System.out.println("Drug Development");
-        System.out.println("@ArulDiya");
         for(i=0;i<slack.length();i++)
         {
             if(slack.charAt(i)!=twitter.charAt(i))
             count++;
         }
-        System.out.println("Hamming Distance: "+count);
+        System.out.println("Diya Arul, diyaarul1234@gmail.com, @DiyaArul, Drug Development, @ArulDiya, "+count);
     }
 }
