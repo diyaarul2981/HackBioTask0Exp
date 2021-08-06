ef5657b33ed08b2029653c44bc41ffcb7e80a8e9
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-06 22:59:51 +0530

    Delete 1.JaykishanPython.py

--- 1.JaykishanPython.py
+++
@@ -1,7 +0,0 @@
-print("Name:            Solanki Jaykishan");
-print("Email:           solankijaykishan91@gmail.com");
-print("Slack_Username:  @Jay");
-print("Biostack:        Drug_Development")
-
-
- 
