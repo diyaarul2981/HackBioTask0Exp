bb95afb2d8347906e1190e464d61116921f2919b
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-07 12:39:11 +0530

    Update Aneela.cpp

--- Aneela.cpp
+++ Aneela.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 int main()
 {
-	cout<<"Aneela	Qasim, aneelaq21@gmail.com , @Aneela , Genomics ,aneela1">>;
-	return O;
+	cout<<"Aneela Qasim, aneelaq21@gmail.com , @Aneela , Genomics ,aneela1">>;
 }
 
