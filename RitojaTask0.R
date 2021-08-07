3a3cbd1b9a99ca2d673c18bccbd3eb635757ed90
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-07 10:47:38 +0530

    Update RitojaTask0.R

--- RitojaTask0.R
+++ RitojaTask0.R
@@ -7,12 +7,13 @@ twitter<-"@RayRitoja25"
 count<-0
 library(stringi)
 for (i in 1:nchar(twitter)){ #as twitter username is longer
-check1<-stri_sub(slack,i,i)
-check2<-stri_sub(twitter,i,i)
-if(check1 != check2){
-count<-count+1
-}#end of if
+	check1<-stri_sub(slack,i,i)
+	check2<-stri_sub(twitter,i,i)
+	if(check1 != check2)
+		{
+		count<-count+1
+	}#end of if
 }#end of for
-output<-data.frame(name,",", email, ",",slack,",", biostack,",", twitter,",", count)
-cat(paste(output,collapse=','))
+#output<-data.frame(name,email,slack,biostack,twitter,count)
+cat(paste(name,email,slack,biostack,twitter,count,sep=", "))
 #end
