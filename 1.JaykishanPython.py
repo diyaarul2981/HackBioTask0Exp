09149de9f513eef834320f3648e318e64d8ecf54
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-07 12:21:43 +0530

    Update 1.JaykishanPython.py

--- 1.JaykishanPython.py
+++ 1.JaykishanPython.py
@@ -1,9 +1,9 @@
 Slack_id = "@Jay"
 Twitter_id = "@Ja9"
-def hamming_distance(Twitter_id, Slack_id):
+def hamming_distance(Slack_id, Twitter_id):
     distance=0
-    L = len(Twitter_id)
+    L = len(Slack_id)
     for i in range(L):
-        if Twitter_id[i] != Slack_id[i]:
+        if Slack_id[i] != Twitter_id[i]:
             distance += 1
-            print("Solanki Jaykishan, solankijaykishan91@gmail.com, @Jay, Drug_Development, @Ja9, " , hamming_distance(Twitter_id, Slack_id))
+print("Solanki Jaykishan, solankijaykishan91@gmail.com, @Jay, Drug_Development, @Ja9, " , hamming_distance(Slack_id, Twitter_id))
