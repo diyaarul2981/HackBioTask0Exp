d6f1291ea42f06c728f40869e74274ed80c21ed4
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-07 12:29:43 +0530

    Update Jaykishan_Ruby.rb

--- Jaykishan_Ruby.rb
+++ Jaykishan_Ruby.rb
@@ -1,1 +1,1 @@
-puts "Solanki Jaykishan, solankijaykishan91@gmail.com, @Jay, Drug_Development, @Jayk, @Jay" <=> "@Ja9";
+puts "Solanki Jaykishan, solankijaykishan91@gmail.com, @Jay, Drug_Development, @Jayk," + " @Jay" <=> "@Ja9";
